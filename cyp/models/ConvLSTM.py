5d544144836916497a7606e6c34c33636bdcbd3d
ezekielbarnett <ezekbarnett@gmail.com>
2019-12-18 20:07:53 +0100

    added framework for convlstm

---
+++ cyp/models/ConvLSTM.py
@@ -0,0 +1,48 @@
+import torch.nn as nn
+from torch.autograd import Variable
+import torch
+import torch.nn.functional as F
+
+
+class ConvLSTMCell(nn.Module):
+
+    def __init__(self, input_size, input_dim, hidden_dim, kernel_size, bias):
+        """
+        Initialize ConvLSTM cell.
+
+        Parameters
+        ----------
+        input_size: (int, int)
+            Height and width of input tensor as (height, width).
+        input_dim: int
+            Number of channels of input tensor.
+        hidden_dim: int
+            Number of channels of hidden state.
+        kernel_size: (int, int)
+            Size of the convolutional kernel.
+        bias: bool
+            Whether or not to add the bias.
+        """
+        super(ConvLSTMCell, self).__init__()
+
+        self.height, self.width = input_size
+        self.input_dim  = input_dim
+        self.hidden_dim = hidden_dim
+
+        self.kernel_size = kernel_size
+        self.padding     = kernel_size[0] // 2, kernel_size[1] // 2
+        self.bias        = bias
+
+        self.conv = nn.Conv2d(in_channels=self.input_dim + self.hidden_dim,
+                              out_channels=4 * self.hidden_dim,
+                              kernel_size=self.kernel_size,
+                              padding=self.padding,
+                              bias=self.bias)
+
+
+    def forward(self, input, prev_state):
+
+        #i =
+        #f =
+        #o =
+        #g = 
